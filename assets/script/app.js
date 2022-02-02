f6bf848eaf266385108cdba4d9b276a957aef230
Oliver Drew <oli.drew92@gmail.com>
2022-02-02 16:36:47 +0000

    Add openlayers map with markers

--- assets/script/app.js
+++ assets/script/app.js
@@ -46,3 +46,47 @@ const renderOutput = (data) => {
 
 // Run Travel Advisor fucntion - Birmingham
 getTravelAPI(52.48142, -1.89983);
+
+// Openlayers Map
+// Icons
+const iconNottingham = new ol.Feature({
+  geometry: new ol.geom.Point(ol.proj.fromLonLat([-2, 53])),
+  name: "Somewhere near Nottingham",
+});
+
+const iconBrum = new ol.Feature({
+  geometry: new ol.geom.Point(ol.proj.fromLonLat([-1.90769, 52.47853])),
+  name: "Somewhere in Brum",
+});
+
+const iconLondon = new ol.Feature({
+  geometry: new ol.geom.Point(ol.proj.fromLonLat([-0.125, 51.50714])),
+  name: "Somewhere in London",
+});
+
+// Map
+const map = new ol.Map({
+  target: "map",
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+    new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [iconNottingham, iconBrum, iconLondon],
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 46],
+          anchorXUnits: "fraction",
+          anchorYUnits: "pixels",
+          src: "./assets/images/icon.png",
+        }),
+      }),
+    }),
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-1.9076915516146231, 52.4785349725096]),
+    zoom: 8,
+  }),
+});
